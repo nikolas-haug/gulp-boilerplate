41471b77b4edabea2cb96cc48bd3524419c1dbc7
Nikolas Haug <nikolas.haug@futurenet.com>
2019-09-26 10:33:53 -0500

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,10 @@
+const { src, dest, watch, series, parallel } = require('gulp'); // specific gulp API functions
+// All other gulp related packages
+const sourcemaps = require('gulp-sourcemaps');
+const sass = require('gulp-sass');
+const concat = require('gulp-concat');
+const uglify = require('gulp-uglify');
+const postcss = require('gulp-postcss');
+const autoprefixer = require('autoprefixer');
+const cssnano = require('cssnano');
+var replace = require('gulp-replace');
