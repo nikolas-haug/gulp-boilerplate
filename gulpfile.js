a1eb78c3175cf1b64ef9ba9d9c0d8bc3809093fc
Nikolas Haug <nikolas.haug@futurenet.com>
2019-09-26 10:37:45 -0500

    add scss task

--- gulpfile.js
+++ gulpfile.js
@@ -8,3 +8,20 @@ const postcss = require('gulp-postcss');
 const autoprefixer = require('autoprefixer');
 const cssnano = require('cssnano');
 var replace = require('gulp-replace');
+
+// Create the file paths here in an object
+const files = {
+    scssPath: 'app/scss**/*.scss',
+    jsPath: 'app/js/**/*.js'
+}
+
+// SCSS task setup
+function scssTask() {
+    return src(files.scssPath)
+        .pipe(sourcemaps.init()) // initialize sourcemaps
+        .pipe(sass()) // compile SCSS to CSS
+        .pipe(postcss([autoprefixer(), cssnano()])) // PostCSS plugins
+        .pipe(sourcemaps.write('.')) // write sourcemaps in current directory
+        .pipe(dest('dist')) // indicate the destination of compiled CSS
+}
+
